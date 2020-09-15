bcc6a4a38a02aa6ee5683749ec702f36704c463e
Reinaldo <reinaldoacdc@gmail.com>
2020-09-15 16:46:28 -0300

    adjust migration create_portfolio

--- db/migrate/20200915193430_create_portfolios.rb
+++ db/migrate/20200915193430_create_portfolios.rb
@@ -2,9 +2,9 @@ class CreatePortfolios < ActiveRecord::Migration[6.0]
   def change
     create_table :portfolios do |t|
       t.string :slug
-      t.boolean :active
-      t.boolean :listed
-      t.boolean :featured
+      t.boolean :active, default: false
+      t.boolean :listed, default: false
+      t.boolean :featured, default: false
       t.integer :temp_avatar
       t.boolean :remote_ok
       t.references :user, null: false, foreign_key: true
