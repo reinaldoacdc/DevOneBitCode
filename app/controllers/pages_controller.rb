290380c77287e78bb43ee1b34a657c0ace57714b
Reinaldo <reinaldoacdc@gmail.com>
2020-09-28 13:02:26 -0300

    cutomize partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
   end
+
+  private
+ 
+  def is_user_logged_in?
+    if current_user
+      redirect_to portfolios_path
+    end
+  end  
 end
