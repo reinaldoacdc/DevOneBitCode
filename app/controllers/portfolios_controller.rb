60f91009760f0dfabae61b1dac42427b2aa9fe05
Reinaldo <reinaldoacdc@gmail.com>
2020-09-28 13:27:34 -0300

    Add portfolios feature

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,9 @@
+lass PortfoliosController < ApplicationController
+  before_action :load_portfolios, only: :index
+ 
+  def index
+      authorize(Portfolio)
+      @portfolios = policy_scope(Portfolio)
+  end
+    
+end
